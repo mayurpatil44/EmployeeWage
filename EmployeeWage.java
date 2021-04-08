17eadd0ad33499a02a2997d501d79e9565268d8c
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-08 14:34:33 +0530

    Check it is Part time Employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,21 +2,23 @@ public class EmployeeWage
 {
    public static void main(String[] args)
 	{
-		int IS_PRESENT=1;
-		int EMP_RATE_PER_HR=20;
-		int empHrs=8;
-		int salary;
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-
-		if(IS_PRESENT == empcheck)
-		{
-			salary = empHrs * EMP_RATE_PER_HR ;
-		}
+		//Constants
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+		int EMP_RATE_PER_HOUR=20;
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+		//Computation
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_PART_TIME)
+			empHrs=4;
+		else if (empCheck == IS_FULL_TIME)
+			empHrs=8;
 		else
-			salary=0;
-
-		System.out.println("Salary: " +salary);
-
+			empHrs=0;
+		empWage=empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
    	}
 }
 
