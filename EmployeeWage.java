6d9ed5365045414cc9dad7d25b24416a4b64f522
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-08 10:40:10 +0530

    Daily EmployeeWage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,23 @@
 public class EmployeeWage
 {
    public static void main(String[] args)
-   {
-      int IS_FULL_TIME = 1;
-      double empcheck = Math.floor(Math.random() * 10) % 2;
-      if(empcheck == IS_FULL_TIME)
-         System.out.println("Employee is present");
-      else
-         System.out.println("Employee is abscent");
-   }
+	{
+		int IS_PRESENT=1;
+		int EMP_RATE_PER_HR=20;
+		int empHrs=8;
+		int salary;
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+
+		if(IS_PRESENT == empcheck)
+		{
+			salary = empHrs * EMP_RATE_PER_HR ;
+		}
+		else
+			salary=0;
+
+		System.out.println("Salary: " +salary);
+
+   	}
 }
+
+
